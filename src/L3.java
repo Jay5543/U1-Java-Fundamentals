cb0d7a55ce845670a12166594adff54f56159320
Jay5543 <floydjer000@warren.k12.in.us>
2021-08-09 12:58:43 -0400

    work

--- src/L3.java
+++ src/L3.java
@@ -1,4 +1,49 @@
 public class L3 {
 
     //add the main method header below.
+
+    /*
+    Literal a Value that was written in the code
+    An expression is a value using veriables or operators
+
+    A magic number is a magic value that doesnt relate to the program or solution
+    Try to avoid MAGIC numbers
+     */
+    //add the main method header below.
+
+    public static void main(String[] args) {
+        int literalValue = 5;
+        int expression = 5+6;
+        int expression2 = literalValue + 6;
+        int expression3 = literalValue + literalValue;
+
+        //Example of Literal Values
+        double literalValue2 = 26.4;
+        boolean literalValue3 = false;
+        String literalValue4 = "people";
+
+        //Examole of Expression - the result of the expresion must matchg the data type
+        double expression4 = 2.4+3.3;
+        boolean expression5 = 5>6;
+
+       // int badExample = 5.4-0.4;
+
+        //String expression is called String CONCATENATION. you can combine strings using the + sign
+
+        String expression6 = "A full house full of " + literalValue4;
+                System.out.println(expression6);
+
+
+
+
+                String message1 = "The store has ";
+                String message2 = " apples in stock.";
+                int numApples = 20;
+
+                System.out.println("The store has 20 apples in stock.");
+                System.out.println("The store has " + numApples + " apples in stock");
+                System.out.println(message1 + numApples + message2);
+
+
+    }
 }
--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,23 @@
 public class L4 {
+
+    public static void main(String[] args) {
+
+        int value = 5;
+
+        //Declare a Variable - stating the datatype and identifier
+
+        double total;
+        String message = "Hi";
+        final double GRAVITY = -9.8;
+
+        //Assign a value
+        total = 6;
+
+        System.out.println(message + ", the value is " + total);
+
+        total = 10;
+        message = "Bye";
+
+        System.out.println(message + ", the value is " + total);
+    }
 }
